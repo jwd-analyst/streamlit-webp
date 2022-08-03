f42b425d4c834d5827d76876f7b602d21a87cb1a
jwd-analyst <jannik.dahle@tutanota.com>
2022-08-03 07:46:46 +0200

    clean and comment code

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import streamlit as st
+import re
+
 from pathlib import Path
 from PIL import Image
 from zipfile import ZipFile
-import re
 
 st.title('Image to WebP Converter')
 
@@ -13,30 +14,32 @@ uploaded_files = st.file_uploader('Choose a Image File',
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
 
-     # st.write("filename:", uploaded_file.name)
-
 quality_value = st.slider('Choose Quality Score', 0, 100, 80)
 
 if st.button('Convert to WebP'):
     if len(uploaded_files) > 0:
-     zipObj = ZipFile('images.zip', 'w')
-     zipObj.write('webp_images/readme.txt')
+        zipObj = ZipFile('images.zip', 'w')  # Create and open archive
+        zipObj.write('webp_images/readme.txt')  # Add readme file to archive
 
-     for file in reversed(uploaded_files):
-          new_file_name = re.sub('\..*', '.webp', str(file.name))
-          new_file_path = 'webp_images/' + new_file_name
-          image = Image.open(file)  # Open image
-          image.save(fp= new_file_path, format='webp',
-                    quality=quality_value)  # Convert image to webp
-          new_file_size = round(Path(new_file_path).stat().st_size / 1000, 2)
-          st.write(new_file_name, new_file_size, 'KB ✅')
-          zipObj.write(new_file_path)
-          Path(new_file_path).unlink()
+        for file in reversed(uploaded_files):
+            new_file_name = re.sub(
+                '\..*', '.webp', str(file.name))  # Set new filename
+            new_file_path = 'webp_images/' + new_file_name  # Set new file path
+            image = Image.open(file)  # Open image
+            image.save(fp=new_file_path, format='webp',
+                       quality=quality_value)  # Convert image to webp
+            new_file_size = round(Path(new_file_path).stat(
+            ).st_size / 1000, 2)  # Calculate new file size
+            # Print success message and file size
+            st.write(new_file_name, new_file_size, 'KB ✅')
+            zipObj.write(new_file_path)  # Add file to archive
+            Path(new_file_path).unlink()  # Delete file
 
-     zipObj.close()
-     
-     with open('images.zip', 'rb') as f:
-          st.download_button('Download Zip', f, file_name='images.zip')  # Defaults to 'application/octet-stream'
+        zipObj.close()  # Close archive
+
+        with open('images.zip', 'rb') as f:
+            # Defaults to 'application/octet-stream'
+            st.download_button('Download Zip', f, file_name='images.zip')
 
     else:
-     st.error('No Files uploaded')
+        st.error('No Files uploaded')  # Error message if no file was uploaded
